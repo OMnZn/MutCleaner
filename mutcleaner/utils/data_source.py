a74a9b4513ea181dcea925270db47d1428b6187c
Mengxin Yang <15373826193@163.com>
2026-05-27 11:09:51 +0800

    Add Chitosanase Dataset to data source

--- mutcleaner/utils/data_source.py
+++ mutcleaner/utils/data_source.py
@@ -323,6 +323,15 @@ DATASETS = {
             },
         },
     },
+    "Chitosanase Dataset": {
+        "paper_title": "Chitosanase_dataset",
+        "official_doi": None,
+        "files": ["Chitosanase_Dataset.csv"],
+        "huggingface_repos": [
+            "datasets/xulab-research/MutCleaner/resolve/main/Chitosanase_Dataset/Chitosanase_Dataset.csv?download=true",
+        ],
+        "file_name": ["Chitosanase_Dataset.csv"],
+    },
 }
 
 
@@ -350,6 +359,7 @@ def list_datasets_with_built_in_cleaners() -> None:
     - CTXM Epistasis Dataset
     - RBD Antibody Dataset
     - RBD ACE2 Dataset
+    - Chitosanase Dataset
     """
     print("Public datasets with ready-to-use cleaning pipelines:")
     for key, info in DATASETS.items():
